95989710d1ee0b977d154587dca54137fb4feae4
Sindre <sndrem@live.no>
2015-10-04 22:41:54 +0200

    Kommet litt på vei

--- app/scripts/scraping.js
+++ app/scripts/scraping.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	fs = require('fs'),
 	request = require('request'),
 	cheerio = require('cheerio'),
+	Firebase = require('firebase'),
 	app = express();
 
 
@@ -14,21 +15,43 @@ app.get('/scrape', function(req, res) {
 		if(!error) {
 			var $ = cheerio.load(html);
 
-			var homeTeam, awayTeam, clock;
+			var homeTeam, awayTeam, time;
+			
 			var matchJson = {
-				homeTeam: "",
-				awayTeam: "",
-				clock: ""
-			};
+						homeTeam: "",
+						awayTeam: "",
+						time: ""
+				};
+
+			var matches = [];
+
+			var ref = new Firebase("https://draktoversikt.firebaseio.com/nextMatches");
 
 			$('#sd_fixtures_table_next tbody tr').filter(function() {
+				matchJson = {
+						homeTeam: "",
+						awayTeam: "",
+						time: ""
+					};
+				
 				var data = $(this);
 
-				$('td').attr('sd_fixtures_home');
-				
+				console.log("Hjemmelag");
+				matchJson.homeTeam = data.children().children()[3].children[0].data;
+				console.log("Bortelag");
+				matchJson.time = data.children().children()[4].children[0].data;
+				matchJson.awayTeam = data.children().children()[5].children[0].data;
 
+				matches.push(matchJson);
+				ref.push(matchJson);			
 			});
+
+			
 		}
+		console.log(matches);
+		fs.writeFile('output.json', JSON.stringify(matches, null, 4), function(err) {
+			console.log('File was written');
+		})
 	})
 
 });
